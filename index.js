5785c424d92c57c06e1ceaebbd04e5a752f686e8
joseph Chun <jjunior.chun@gmail.com>
2019-01-18 00:36:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (someValue < 400) {
    return "This one is on me!"
 } else if (someValue > 2000){
   return "I will gladly take your thirty bucks."
-} else if (someValue >+ 2500){
+} else if (someValue >= 2500){
   return "No can do."
 }
 }
