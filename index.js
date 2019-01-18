e60bd9e0d9158b30d595d6e729bc8ce1ef1d79fb
joseph Chun <jjunior.chun@gmail.com>
2019-01-18 00:38:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 function scuberGreetingForFeet(someValue){
   
-if (someValue > 2000) {
-   return  "I will gladly take your thirty bucks."
+if (someValue > 2500) {
+   return "No can do."
 } else if (someValue < 400){
   return "This one is on me!" 
 } 
